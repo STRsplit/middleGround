f5eea54f2184ae63e3d2d0ebd0bc1abca9da70a1
Streed12 <steve.t.reed@gmail.com>
2019-08-11 10:33:30 -0500

    Updated code to center element

---
+++ index.js
@@ -0,0 +1,16 @@
+export default (string, element) => {
+    if (!element) {
+        return string;
+    }
+    const [ even, mid ] = string.length % 2 === 0 ? [true, (string.length / 2) - 1] : [false, Math.floor(string.length / 2)];
+    const finalOutput = string.split('').reduce((result, char, ind) => {
+        if (ind === mid) {
+            result = even ? [ ...result, '\u00A0', element ] : [ ...result, element ];
+        } else {
+            result = [ ...result, '\u00A0' ];
+        }
+        return result;
+    }, []);
+    
+    return finalOutput;
+}
